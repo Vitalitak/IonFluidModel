4ecd2c3253acc0dd0ac1ce8b83554d7a16e0a811
Vitalitak <kuzmenko.vo@phystech.edu>
2023-03-30 16:08:37 +0300

    stationary sheath initial conditions for RF bias

--- stationary.py
+++ stationary.py
@@ -13,71 +13,10 @@ dy/dx=f(x, y)
 Poisson equation with Maxwell-Boltzmann electrons and ion concentration from fluid model
 for dn/dt = 0 and du/dt = 0
 
-N=3
-gammai = 5/3
-
 dPsi/dx=F(x, Psi)
 
 """
 
-"""
-def RKPois1(dx, Psi, Nsh, n0, Ti, Te, V0):
-    e = 1.6E-19
-    eps0 = 8.85E-12
-    kTe = Te * 1.6E-19  # J
-
-    
-    Psi(0)=0
-    dPsi/dx(0) = 0
-    dPsi/dx<0
-
-    boundary N(x) = 0.995
-
-    dPsi/dx=F(x, Psi)
-    F = -(A*exp(Psi)+B*Psi+C*(1-19*Te/2/Ti*Psi)^3/2+D)^1/2
-
-    A=2*e*e*n0/eps0/kTe*m.exp(e*V0/kTe)
-    B=-32/19*e*e*n0/eps0/kTe
-    C=8/361*Ti/Te*e*e*n0/eps0/kTe
-    D=-2*e*e*n0/eps0/kTe*m.exp(e*V0/kTe)-8/361*Ti/Te*e*e*n0/eps0/kTe
-    Ksi(0)=0
-
-    Four order Runge-Kutta method
-    f1=F(x[n], Psi[n])
-    f2=F(x[n]+dx/2, Psi[n]+dx/2*f1)
-    f3=F(x[n]+dx/2, Psi[n]+dx/2*f2)
-    f4=F(x[n]+dx, Psi[n]+dx*f3)
-
-    Psi[n+1]=Psi[n]+dx/6*(f1+2*f2+2*f3+f4)
-
-    
-
-    # dx = x[Npl - 1]-x[Npl - 2]
-    # Nx = len[Ksi]
-
-    Psi[2] = dx * dx * e * e * n0 / eps0 / kTe * (m.exp(e * V0 / kTe) - 1)
-    A = 2 * e * e * n0 / eps0 / kTe
-    B = -32 / 19 * e * e * n0 / eps0 / kTe
-    C = 8 / 361 * Ti / Te * e * e * n0 / eps0 / kTe
-    D = -2 * e * e * n0 / eps0 / kTe - 8 / 361 * Ti / Te * e * e * n0 / eps0 / kTe
-
-    # print(A)
-    # print(B)
-    # print(C)
-    # print(D)
-
-    for i in range(2, Nsh):
-        f1 = -m.pow((A * m.exp(Psi[i]) + B * Psi[i] + C * m.pow((1 - 19 * Te / 2 / Ti * Psi[i]), 1.5) + D), 0.5)
-        f2 = -m.pow((A * m.exp(Psi[i] + dx / 2 * f1) + B * (Psi[i] + dx / 2 * f1) + C * m.pow(
-            (1 - 19 * Te / 2 / Ti * (Psi[i] + dx / 2 * f1)), 1.5) + D), 0.5)
-        f3 = -m.pow((A * m.exp(Psi[i] + dx / 2 * f2) + B * (Psi[i] + dx / 2 * f2) + C * m.pow(
-            (1 - 19 * Te / 2 / Ti * (Psi[i] + dx / 2 * f2)), 1.5) + D), 0.5)
-        f4 = -m.pow((A * m.exp(Psi[i] + dx * f3) + B * (Psi[i] + dx * f3) + C * m.pow(
-            (1 - 19 * Te / 2 / Ti * (Psi[i] + dx * f3)), 1.5) + D), 0.5)
-        Psi[i + 1] = Psi[i] + dx / 6 * (f1 + 2 * f2 + 2 * f3 + f4)
-
-    return Psi
-"""
 
 def RKPoisN(dx, Psi, Nsh, Nx, n0, Ti, Te, Psil, FN):
     e = 1.6E-19
@@ -154,21 +93,18 @@ def main():
     eps0 = 8.85E-12
 
     # plasma parameters
-    Te = 2.5  # eV
+    Te = 2.3  # eV
     Ti = 0.06  # eV
-    n0 = 5E17  # m-3
-    Vdc = -12.6
+    n0 = 4E17  # m-3
+    Vdc = -12
     C = 1.4E-16
     C /= 1.6E-19
     gamma = 5/3
 
-    # stitching parameters
-    P = 0.995  # P = ni(a)/n0 boundary N(x)
-    b = 8
 
     kTi = Ti * 1.6E-19  # J
     kTe = Te * 1.6E-19  # J
-    V0 = 0
+    #V0 = 0
     # V0 = kTe / e * (1 - P) / (m.cosh(m.sqrt(e * e * n0 / 2 / eps0 / kTi) * a) - 1)
 
     Nt = int(tEnd / dt)
@@ -185,28 +121,16 @@ def main():
     Vrf = 0
 
     Psil = e*Vdc/kTe
-    #NPsi = 1000
-    #dPsi = Psil/NPsi
 
     FPsi = lambda x: (5*gamma-3)*Ti/Te/2/(gamma-1)*(1-3*(gamma-1)/(5*gamma-3)*m.pow(x, -2)-2*gamma/(5*gamma-3)*m.pow(x, gamma-1))
-
-    #Number = FPsi(0.01)
-    #print(Number)
 
     FN = inversefunc(FPsi, domain=[0.001, 1])
 
     """
-    xPsi = [k * dPsi for k in range(0, NPsi)]
-
-    xNi = [0 for k in range(0, NPsi)]
-    intNx = [0 for k in range(0, NPsi)]
-
     res, err = quad(FN, 0, -1)
     print(print("The numerical result is {:f} (+-{:g})"
     .format(res, err)))
     print(quad(FN, 0, -1)[0])
-
-    #Psi = RKPois1(dx, Psi, Nsh, n0, Ti, Te, V0)
     """
 
     Psi = RKPoisN(dx, Psi, Nsh, Nx, n0, Ti, Te, Psil, FN)
@@ -215,9 +139,6 @@ def main():
         #xNi[i] = FN(xPsi[i])
         #intNx[i] = quad(FN, 0, xPsi[i])[0]
 
-
-    #for i in range(0, Nsh):
-        #Ni[i] = 1 + 3 / 19 * (1 - m.sqrt(1 - 19 * Te / 2 / Ti * Psi[i]))
 
     for i in range(Nsh, Nx):
         Ni[i] = FN(Psi[i])
@@ -230,14 +151,8 @@ def main():
         ni[i] = Ni[i]*n0
         ne[i] = n0*m.exp(e*V[i]/kTe)
         ui[i] = n0 * m.sqrt(kTi / mi) / ni[i]
-    """
-    plt.plot(xPsi, xNi)
-    plt.ylabel('Ni')
-    plt.show()
-    plt.plot(xPsi, intNx)
-    plt.ylabel('intNx')
-    plt.show()
-    """
+
+
     plt.plot(x, Psi)
     plt.ylabel('Psi')
     plt.show()
@@ -263,15 +178,6 @@ def main():
     plt.ylabel('u')
     plt.show()
 
-    #print(FN(Number))
-    """
-    f = symbols('f', cls=Function)
-    #f, g = symbols("f g", cls=Function)
-    x = symbols("x")
-    #init_printing(use_unicode=True)
-    diffeq = Eq(f(x).diff(x, x)-e*e*n0/eps0/kTe*m.exp(e*V0/kTe)*exp(f(x)), 0)
-    dsolve(diffeq, f(x), hint='linear_coefficients', ics={f(0): 0, f(x).diff(x).subs(x, 0): 0})
-    """
     return 0
 
 
