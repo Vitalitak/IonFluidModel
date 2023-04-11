95124f4c1bb3d933ecf69248b0344f0ab523e39c
Vitalitak <kuzmenko.vo@phystech.edu>
2023-04-11 17:40:34 +0300

    dynamic: initial conditions for electron velocity

--- dynamic.py
+++ dynamic.py
@@ -237,7 +237,9 @@ def momentum_e(V, n, uprev, kTe, de, n0, Nel, Nx, dt):
     for i in range(1, Nel):
         u[i] = uprev[i]+dt*(kTe/me*(Psi[i]-Psi[i-1]) /dx - kTe/me*m.pow(N[i], gamma-2)*(N[i]-N[i-1])/dx-(uprev[i]*uprev[i]-uprev[i-1]*uprev[i-1])/2/dx)
 
-    #print(u[Nel-1])
+    print(kTe/me*(Psi[Nel-1]-Psi[Nel-2]) /dx)
+    #print(kTe/me*m.pow(N[Nel-1], gamma-2)*(N[Nel-1]-N[Nel-2])/dx)
+    #print((uprev[Nel-1]*uprev[Nel-1]-uprev[Nel-2]*uprev[Nel-2])/2/dx)
 
 
     return u
@@ -429,8 +431,8 @@ def main():
     #for i in range(0, Nel):
         #ne_1[i] = n0*m.exp(e*V_1[i]/kTe)
 
-    for i in range(2, 200):
-        print(q)
+    for i in range(2, 250):
+        #print(q)
         #Vel2 = V[Nel-1] - 10 * m.sin(13560000 * 2 * m.pi * i / 2 * dt)+q
         Vel2 = V[Nel-1] + q
 
