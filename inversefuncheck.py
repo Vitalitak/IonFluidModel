8a8d866116fb13e6ab1ce375df686511eedc592a
Vitalitak <kuzmenko.vo@phystech.edu>
2023-06-06 12:35:30 +0300

    inverse function check

--- inversefuncheck.py
+++ inversefuncheck.py
@@ -11,19 +11,23 @@ def main():
     Te = 2.68  # eV
     Ti = 0.05  # eV
     dPsi = -0.01
+    dN = 0.001
     #Psi0 = -0.5
     V0 = -2.68
-    Psi0 = V0/Te
+    Psi0 = 1e-7
 
     FPsi = lambda x: (5 * gamma - 3) * Ti / Te / 2 / (gamma - 1) * (
                 1 - 3 * (gamma - 1) / (5 * gamma - 3) * m.pow(x, -2) - 2 * gamma / (5 * gamma - 3) * m.pow(x,
-                                                                                                           gamma - 1)) + V0/Te
+                                                                                                           gamma - 1)) + Psi0
     FN = inversefunc(FPsi, domain=[0.001, 1])
 
     Ni = [0 for k in range(0, 1000)]
     Ne = [0 for k in range(0, 1000)]
     Psi = [k * dPsi+Psi0 for k in range(0, 1000)]
     V = [0 for k in range(0, 1000)]
+    N = [1-k*dN for k in range(0, 100)]
+    Psii = [0 for k in range(0, 100)]
+    Psie = [0 for k in range(0, 100)]
 
     for i in range(0, 1000):
         Ni[i] = FN(Psi[i])
@@ -31,10 +35,20 @@ def main():
         Ne[i] = m.exp(Psi[i])
         V[i] = Psi[i]*Te
 
+    for i in range(0, 100):
+        Psii[i] = FPsi(N[i])
+        Psie[i] = np.log(N[i])
+
     plt.plot(V, Ne, 'b')
     plt.plot(V, Ni, 'r')
     plt.ylabel('N')
     plt.xlabel('V')
+    plt.show()
+
+    plt.plot(N, Psii, 'r')
+    plt.plot(N, Psie, 'b')
+    plt.ylabel('Psi')
+    plt.xlabel('N')
     plt.show()
 
     return 0
